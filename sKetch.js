29cee0d02006930be4ed3b7f4d8114a6589c0982
Samuquinha3A <00001091342672SP@al.educacao.sp.gov.br>
2024-09-03 09:28:11 -0300

    adicionamdo style e sKetch

---
+++ sKetch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(400, 400);
+  background("salmon");
+    }
+  function draw() {
+    stroke("baby pink");
+    fill("black");
+  
+  
+   if(mouseIsPressed) {
+    rect (mouseX, mouseY, 20, 45,);
+     } 
+  }
+  
